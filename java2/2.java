5ac7e6d23a5d6468971fb83ee1d87578304af3b4
Maxim <supreme14@rambler.ru>
2022-10-18 19:19:02 +0300

    2

---
+++ java2/2.java
@@ -0,0 +1,17 @@
+package java2;
+class program 
+{
+    public static void main(String[] args) 
+    {
+        int a = 1000;
+        for (int i = 0; i < a; i++) 
+        {
+            if (i%2==1)
+            {
+                System.out.println(i);
+            }  
+        
+        }
+    
+    }
+}
